ad08a6a9aeccdd24668b49e3b354f279b93de3f0
Richard Goluszka <richardjgoluszka@lewisu.edu>
2021-04-27 18:11:44 -0500

    Timer component caused Github Action issues, removed and replaced with TaskItem for testing

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 
-function Timer({seconds, onFinish}) {
-	//remaining tracks the remaining time on the timer
-	const [remaining, setRemaining] = useState(seconds);
-	//trigger tracks the expiration of the timer
-	const [trigger, setTrigger] = useState(false);
-
-	/* Sets up and breaks down the timer tick rate in browser */
-	useEffect( () => {
-		let timerID = setInterval(() => tick(), 1000);
-		return () => clearInterval(timerID);
-	});
-
-	/* Updates the state of remaining and trigger */
-	const tick = () => {
-		if (remaining -1 >= 0 && !trigger) {
-			setRemaining(remaining - 1);
-		} else if (!trigger) {
-			setTrigger(true);
-			onFinish();
-			setRemaining(remaining + 1);
-		} else {
-			setRemaining(remaining + 1);
-		}
-	}
-
+function TaskItem({itemNo, expired, text, action}) {
 	return (
-		<p>
-			{trigger ? `${remaining} seconds late` : `${remaining} seconds left`}
-		</p>
-	);
+		<button
+			className='taskitem'
+			onClick={action}
+			style={{
+				backgroundColor: expired ? 'red' : 'white',
+				color: expired ? 'yellow' : 'black'
+			}}
+		>
+			{text}
+		</button>
+	)
 }
 
 export default function App() {
-	const name = 'world';
-	return (<Timer seconds='30' onFinish={() => alert('done')}/>);
+	return (
+		<TaskItem itemNo={1} expired={false} text='sample' action={f => f} />
+	);
 }
