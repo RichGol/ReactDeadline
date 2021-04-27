cde61a55870eeb630c81123266f4a926024a8528
Richard Goluszka <richardjgoluszka@lewisu.edu>
2021-04-27 18:48:10 -0500

    Github action success with Mosaic-v2, but it uses the useState() hook? Testing again with FinalProjectApp

--- src/App.js
+++ src/App.js
@@ -1,195 +1,225 @@
-//https://nice-forest-0da45c110.azurestaticapps.net/
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
-const createArray = length => [...Array(length)];
-
-/***********************************
- * RENDERS THE MosaicBtn COMPONENT *
- ***********************************/
-function MosaicBtn() {
-	//setup statefulness for interaction, shape, text, and colors
-	const [clicked, setClicked] = useState(false);
-	const [radius, setRadius] = useState('0px');
-	const [text, setText] = useState('#');
-	const [color, setColor] = useState('0,0,0');
-	const [bgColor, setBGColor] = useState('255,255,255');
-
-	//randomize from default values on first render
-	useLayoutEffect(() => {
-		setRadius(genRadius());
-		setText(genLetter());
-
-		let textColor = genColor();
-		let bgColor = genColor();
-		while (!validateColors(textColor, bgColor)) {
-			textColor = genColor();
-			bgColor = genColor();
-		}
-		setColor(textColor);
-		setBGColor(bgColor);
-	}, []);
-
-	//generate a random uppercase letter for text state
-	const genLetter = () => {
-		let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
-		'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-		let letterIndex = Math.round(Math.random()*(letters.length - 1));
-		return letters[letterIndex];
-	}
-
-	//generate a css border-radius value of 0px or 50px for shape state
-	const genRadius = () => {
-		let radius = Math.round(Math.random())*50;
-		return `${radius}px`;
-	}
-
-	//generate a random rgb(---) color for color and bgColor state
-	const genColor = () => {
-		let colorArray = Array(3).fill(0).map(
-			x => x = Math.round(Math.random()*255)
-		);
-		return colorArray.toString();
-	}
-
-	//determine whether text and bg colors contrast well before setting state
-	const validateColors = (text, bg) => {
-		//converts RGB values to linear values in range [0, 1]
-		let getColor = x => x / 255;
-
-		//applies gamma correction to each color value
-		let gammaCorrect = x => {
-			if (x < 0.03928) {
-				return x / 12.92;
-			} else {
-				let mod = (x + 0.055) / 1.055;
-				return (mod**2.4);
-			}
-		}
-		//convert RGB strings to arrays, call getColor() and gammaCorrect() on each
-		let textColor = text.split(',').map(getColor).map(gammaCorrect);
-		let bgColor = bg.split(',').map(getColor).map(gammaCorrect);
-		
-		//calculates the luminance of each color
-		let calcLum = color => {
-			return (color[0]*0.2126) + (color[1]*0.7152) + (color[2]*0.0722);
-		}
-		//transform gamma-corrected colors to luminance values
-		let textLum = calcLum(textColor);
-		let bgLum = calcLum(bgColor);
-
-		//calculate contrast between the luminance values
-		let score = (textLum > bgLum) ? (textLum + 0.05) / (bgLum + 0.05) : (bgLum + 0.05) / (textLum + 0.05);
-
-		//compare contrast score against passing value, return result
-		return (score > 3 ? true : false);
-	}
-
-	//renders a button element styled according to the component state
-	return (
-		<button 
-			className='box'
-			style={{
-				backgroundColor: (clicked) ? 'yellow' : `rgb(${bgColor})`,
-				color: (clicked) ? 'black' : `rgb(${color})`,
-				borderRadius: (clicked) ? '50px' : radius
-			}}
-			onClick={() => setClicked(!clicked)}
-		>
-			{(clicked) ? ':)' : text}
-		</button>
-	);
-}
-
-/***********************************
- * RENDERS THE MosaicRow COMPONENT *
- ***********************************/
- function MosaicRow() {
-	//render 12 MosaicBtn Components within a <div>
-	return (
-		<div>
-			{createArray(12).map( (b, i) => (
-				<MosaicBtn key={i} />
-			))}
-		</div>
-	);
-}
-
-/******************************** 
- * RENDERS THE Mosaic COMPONENT *
- ********************************/
- function Mosaic() {
-	//render 12 MosaicRow Components
-	return (
-		<>
-			{createArray(12).map( (r, i) => (
-				<MosaicRow key={i} />
-			))}
-		</>
-	);
-}
-
-/*******************************
- * RENDERS THE COUNTDOWN TIMER *
- *******************************/
- function Clock({ onRefresh, secondCount }) {
-	//setup statefulness for timer countdown
-	const [time, setTime] = useState(secondCount);
+/* HANDLES VIEW OF REMAINING TIME */
+function Timer({seconds, onFinish})  {
+	//remaining tracks the remaining time on the timer
+	const [remaining, setRemaining] = useState(seconds);
+	//trigger tracks the expiration of the timer
 	const [trigger, setTrigger] = useState(false);
 
-	//make timer tick once every second
-	useEffect(() => {
+	/**********************************************
+	 * Handles setting up and breaking down timer *
+	 * tick rate in the browser window.           *
+	 **********************************************/
+	useEffect( () => {
 		let timerID = setInterval(() => tick(), 1000);
 		return () => clearInterval(timerID);
 	});
 
-	//timer behavior for finishing countdown
+	/*******************************************
+	 * Handles updating state of remaining and *
+	 * trigger.                                *
+	 *******************************************/
 	const tick = () => {
-		if (time - 1 >= 0) {
-			setTime(time - 1);
+		if (remaining - 1 >= 0 && !trigger) {
+			setRemaining(remaining - 1);
 		} else if (!trigger) {
-			setTime(secondCount);
 			setTrigger(true);
+			onFinish();
+			setRemaining(remaining + 1);
+		} else {
+			setRemaining(remaining + 1);
 		}
 	}
 
-	//randomize page on finishing countdown
-	const refresh = () => {
-		setTrigger(false);
-		onRefresh();
-	}
-
-	//render the timer countdown
 	return (
-		<h1
-			style={{
-				color: (time <= 10) ? 'red' : 'black'
-			}}
-		>
-			{(trigger) ? refresh() : `${time} seconds left`}
-		</h1>
+		<p>
+			{trigger ? `${remaining} seconds late` : `${remaining} seconds left`}
+		</p>
 	);
 }
 
-/*********************************
- * RENDERS THE WHOLE APPLICATION *
- *********************************/
- export default function App() {
-	//setup statefulness for updating the page
-	const [updates, setUpdates] = useState(0);
+/* HANDLES VIEW OF TASK DESCRIPTION */
+function TaskItem({itemNo, expired, text, action}) {
+	return (
+		<button
+			className='taskitem'
+			onClick={action}
+			style={{
+				backgroundColor: expired ? 'red' : 'white',
+				color: expired ? 'yellow' : 'black'
+			}}
+		>
+			{text}
+		</button>
+	);
+}
 
-	const refresh = () => setUpdates(updates + 1);
+/* HANDLES VIEW OF A TASK */
+function Task({itemNo, text, expiration, remove}) {
+	//expired tracks the expiration status of a task
+	const [expired, setExpired] = useState(false);
 	
-	//render the timer, mosaic, and randomize button
+	/*********************************************
+	 * Handles modifying expired state when task *
+	 * expires.                                  *
+	 *********************************************/
+	const expire = () => setExpired(true);
+
+	return (
+		<div>
+			<TaskItem 
+				itemNo={itemNo}
+				expired={expired}
+				text={text}
+				action={() => alert(`Task: ${text}\nExpired: ${expired}`)}
+			/>
+			<RemoveTask remove={remove} itemNo={itemNo}/>
+			<Timer seconds={expiration} onFinish={expire}/>
+		</div>
+	);
+}
+
+/* HANDLES LOGIC AND VIEW OF ADDING TASKS */
+function AddTask({add}) {
+	//taskDesc is used to refer to the description form input element
+	const taskDesc = useRef();
+	//taskExpir is used to refer to the expiration form input element
+	const taskExpir = useRef();
+
+	/* const datePattern = '(0?[0-9]|1[0-2])[/-](0?[0-9]|[12][0-9]|3[01])[/-]([0-9]{4})';
+	const timePattern = '(0?[1-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])( AM| PM)?';
+	const regexPat = `(${datePattern}) +(${timePattern})`; */
+
+	/********************************************************
+	 * Processes the submission of the HTML form element,   *
+	 * enabling us to add tasks with custom names and times *
+	 ********************************************************/
+	const submit = e => {
+		e.preventDefault();
+		const desc = taskDesc.current.value;
+		const expir = taskExpir.current.value;
+		add(desc, expir);
+		//console.log(expir, new Date(expir));
+		taskDesc.current.value = '';
+		taskExpir.current.value = '';
+		taskDesc.current.focus();
+	}
+
 	return (
 		<>
-			<div>
-				<button onClick={() => refresh()} >
-					Randomize!
-				</button>
-				<Clock key={updates+1} onRefresh={refresh} secondCount={60}/>
-			</div>
-			<Mosaic key={updates} />
+			<form onSubmit={submit}>
+				<p>Task Name:</p>
+				<input ref={taskDesc} type='text' placeholder='task name' required />
+				<p>Expiration Date/Time:</p>
+				<input ref={taskExpir} type='text' /* pattern={regexPat} */
+				placeholder='MM/DD/YYYY hh:mm:ss' required />
+				<input type='submit' id='submit' value='Add Task' />
+			</form>
+		</>
+	);
+}
+
+/* HANDLES LOGIC AND VIEW OF REMOVING TASKS */
+function RemoveTask({remove, itemNo}) {
+	return (
+			<button 
+				className='removetask'
+				onClick={() => remove(itemNo)}
+			>
+				Remove
+			</button>
+	)
+}
+
+/* RENDERS THE WHOLE APPLICATION */
+export default function App() {
+	//tasks state manages the current set of tasks
+	const [tasks, setTasks] = useState(new Array());
+	//nextItem state tracks the number of active tasks
+	const [nextItem, setNextItem] = useState(1);
+	//keyVal state provides a unique key for each task instance
+	const [keyVal, setKeyVal] = useState(0);
+
+	/********************************************
+	 * Loads three Task objects on first render *
+	 ********************************************/
+	useEffect( () => {
+		const temp = [{
+				key: 0,
+				itemNo: 1,
+				text: 'task 1',
+				expiration: 10
+			},{
+				key: 1,
+				itemNo: 2,
+				text: 'task 2',
+				expiration: 20
+			},{
+				key: 5,
+				itemNo: 6,
+				text: 'task 3',
+				expiration: 30
+		}];
+		loadTasks(temp);
+	}, []);
+
+	/*******************************************
+	 * Handles loading tasks from an array of  *
+	 * objects, eventually to load saved tasks *
+	 * from a file.                            *
+	 *******************************************/
+	const loadTasks = temp => {
+		//update keyVal state, preventing overlap with other tasks
+		const lastKey = temp[temp.length - 1].key;
+		setKeyVal(lastKey+1);
+		//update nextItem state, preventing overlap with other tasks
+		const lastItemNo = temp[temp.length - 1].itemNo;
+		setNextItem(lastItemNo+1);
+		//update tasks state with loaded tasks
+		temp.map((task, i) => {
+			setTasks(allTasks => [...allTasks, task]);
+		});
+	}
+
+	/************************************************
+	 * Handles appending a task to the tasks state, *
+	 * updating nextItem automatically              *
+	 ************************************************/
+	const addTask = (text, expiration, key=keyVal, itemNo=nextItem) => {
+		const item = {
+			key: key,
+			itemNo: itemNo,
+			text: text,
+			expiration: expiration
+		};
+		setKeyVal(keyVal + 1);
+		setNextItem(nextItem + 1);
+		setTasks(allTasks => [...allTasks, item]);
+	}
+
+	/************************************************************
+	 * Handles popping the specified task from the tasks state, *
+	 * updating nextItem and tasks automatically                *
+	 ************************************************************/
+	const removeTask = (itemNo) => {
+		let newTasks = tasks.filter( (task, i) => !(task.itemNo === itemNo))
+		.map( (task, i) => {
+			if (task.itemNo !== i+1) {
+				task.itemNo = i+1;
+			}
+			return task;
+		});
+		setTasks(newTasks);
+		setNextItem(nextItem - 1);
+	}
+
+	return (
+		<>
+			<AddTask add={addTask} />
+			{tasks.map( task => (
+				<Task remove={removeTask} {...task} />
+			))}
 		</>
 	);
 }
