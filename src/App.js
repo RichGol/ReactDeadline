f32b0dd54a2d70d8820e78e8996a4a5148e9bf98
Richard Goluszka <richardjgoluszka@lewisu.edu>
2021-04-27 18:19:15 -0500

    Github action successful, adding Task component to test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+/* handles the view of a task description */
 function TaskItem({itemNo, expired, text, action}) {
 	return (
 		<button
@@ -15,8 +16,26 @@ function TaskItem({itemNo, expired, text, action}) {
 	)
 }
 
+function Task({itemNo, text, expiration, remove}) {
+	//expired tracks the status of a task
+	const [expired, setExpired] = useState(false);
+
+	const expire = () => setExpired(true);
+
+	return (
+		<div>
+			<TaskItem 
+				itemNo={itemNo}
+				expired={expired}
+				text={text}
+				action={() => alert(`Task: ${text}\nExpired: ${expired}`)}
+			/>
+		</div>
+	);
+}
+
 export default function App() {
 	return (
-		<TaskItem itemNo={1} expired={false} text='sample' action={f => f} />
+		<Task itemNo={1} expiration={false} text='sample' remove={f => f} />
 	);
 }
