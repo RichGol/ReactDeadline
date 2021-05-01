4abda893bfb857b6fbf4b3488d1c82ced871c2dc
Richard Goluszka <richardjgoluszka@lewisu.edu>
2021-05-01 16:08:33 -0500

    added asynchronous fetch requests to Bryan's Go API to interact with the MongoDB database

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function Timer({dateString, onFinish})  {
 
 	//sets up and breaks down the timer tick rate in the browser window
 	useEffect( () => {
-		let timerID = setInterval(() => tick(), 1000);
+		let timerID = setInterval(tick, 1000);
 		return () => clearInterval(timerID);
 	});
 
@@ -39,17 +39,25 @@ function Timer({dateString, onFinish})  {
 		secs -= (hours * 3600);
 		const mins = Math.floor(secs / 60);
 		secs -= (mins * 60);
-		return `${days} Day(s) ${hours} Hour(s) ${mins} Minute(s) ${secs} Second(s)`;
+
+		if (days !== 0) {
+			return `${days} Day(s) ${hours} Hour(s) ${mins} Minute(s) ${secs} Second(s)`;
+		} else if (days === 0 && hours !== 0) {
+			return `${hours} Hour(s) ${mins} Minute(s) ${secs} Second(s)`;
+		} else if (days === 0 && hours === 0 && mins !== 0) {
+			return `${mins} Minute(s) ${secs} Second(s)`;
+		}
+		return `${secs} Second(s)`;
 	}
 
 	return (
 		<p title={`Due: ${dateString}`}>
-			({trigger ? `${displayRemaining()} late` : `${displayRemaining()} left`})
+			{trigger ? `${displayRemaining()} late` : `${displayRemaining()} left`}
 		</p>
 	);
 }
 
-/* RENDERS THE DESCRIPTION OF TASKS */
+/* RENDERS TASK DESCRIPTIONS */
 function TaskItem({expired, text, action}) {
 	return (
 		<button
@@ -91,8 +99,8 @@ function AddTask({add}) {
 	const taskExpir = useRef();
 
 	//these patterns restrict user form entries to match date and time standards
-	const datePattern = '(0?[0-9]|1[0-2])[/-](0?[0-9]|[12][0-9]|3[01])[/-]([0-9]{4})';
-	const timePattern = '(0?[1-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])( [AaPp][Mm])?';
+	const datePattern = '(0?[1-9]|1[0-2])[/-](0?[1-9]|[12][0-9]|3[01])[/-]([0-9]{4})';
+	const timePattern = '(0?[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])( [AaPp][Mm])?';
 	const regexPat = `(${datePattern}) +(${timePattern})`;
 	//time and timeString pre-fills the expiration form input with the current date
 	const time = new Date();
@@ -110,15 +118,15 @@ function AddTask({add}) {
 	}
 
 	return (
-			<form onSubmit={submit}>
-				<p>Task:</p>
-				<input ref={taskDesc} type='text' placeholder='task name' title='task name' required />
-				<p>Expiration: </p>
-				<input ref={taskExpir} type='text' placeholder='date/time' pattern={regexPat}
-				defaultValue={timeString}
-				title='Format: MM/DD/YYYY hh:mm:ss' required />
-				<input type='submit' id='submit' value='Add Task' />
-			</form>
+		<form onSubmit={submit}>
+			<p>Task:</p>
+			<input ref={taskDesc} type='text' placeholder='task name' title='task name' required />
+			<p>Expiration: </p>
+			<input ref={taskExpir} type='text' placeholder='date/time' pattern={regexPat}
+			defaultValue={timeString}
+			title='Format: MM/DD/YYYY hh:mm:ss' required />
+			<input type='submit' id='submit' value='Add Task' />
+		</form>
 	);
 }
 
@@ -138,7 +146,7 @@ function RemoveTask({remove, itemNo}) {
 function RenderApp({user}) {
 	//tasks stores an array of current tasks
 	const [tasks, setTasks] = useState([]);
-	//keyVal state provides a unique key for each task instance
+	//keyVal provides a unique key for each created task
 	const [keyVal, setKeyVal] = useState(0);
 
 	// Loads three Task objects on first render
@@ -147,17 +155,17 @@ function RenderApp({user}) {
 				key: 0,
 				itemNo: 1,
 				text: 'task 1',
-				expiration: '4/27/2021 22:00:00'
+				expiration: '5/6/2021 10:30:00'
 			},{
 				key: 1,
 				itemNo: 2,
 				text: 'task 2',
-				expiration: '4/27/2021 23:00:00'
+				expiration: '5/6/2021 11:30:00'
 			},{
-				key: 5,
-				itemNo: 6,
+				key: 2,
+				itemNo: 3,
 				text: 'task 3',
-				expiration: '4/28/2021 00:00:00'
+				expiration: '5/6/2021 12:30:00'
 		}];
 		loadTasks(temp);
 	}, []);
@@ -172,7 +180,7 @@ function RenderApp({user}) {
 	}
 
 	//appends a new task to the tasks array
-	const addTask = (text, expiration, key=keyVal, itemNo=tasks.length) => {
+	const addTask = async (text, expiration, key=keyVal, itemNo=tasks.length) => {
 		const item = {
 			key: key,
 			itemNo: itemNo,
@@ -181,6 +189,11 @@ function RenderApp({user}) {
 		};
 		setKeyVal(keyVal + 1);
 		setTasks(allTasks => [...allTasks, item]);
+		/* await fetch('http://127.0.0.1:80/add-task', {
+			method: "POST",
+			mode: "no-cors",
+			body: JSON.stringify({user: user, tasks: item.text, timer: item.expiration})
+		}); */
 	}
 
 	//removes the specified task from the tasks array
@@ -212,10 +225,16 @@ function EntryPage({setUser}) {
 	const user = useRef();
 
 	//processes the submission of the HTML form element, saving the username
-	const submit = e => {
+	const submit = async e => {
 		e.preventDefault();
-		setUser(user.current.value);
+		const name = user.current.value
+		setUser(name);
 		user.current.value = '';
+		/* await fetch('http://127.0.0.1/add-user', {
+			method: "POST",
+			mode: "no-cors",
+			body: JSON.stringify({user: name}),
+		}); */
 	}
 
 	return (
